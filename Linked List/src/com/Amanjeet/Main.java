7ae42d1ec95f95c258648f10602c871ec247c666
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-03-01 15:43:10 +0530

    done with doubly linked list

--- Linked List/src/com/Amanjeet/Main.java
+++ Linked List/src/com/Amanjeet/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("will start learning linked list from here.");
-        Node list = new Node();
+        System.out.println("done with doubly linked list property.");
     }
 }
